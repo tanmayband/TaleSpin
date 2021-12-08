2d9851ce09dce443396133d96cd55e350d7e105f
Tanmay Band <tanmay.band@m56studios.com>
2021-12-09 01:02:23 +0530

    custom buttons on accordion

--- client/src/js/singleAccordion.js
+++ client/src/js/singleAccordion.js
@@ -1,35 +1,62 @@
-import { Accordion, Col, Container, Row } from "react-bootstrap";
+import { Accordion, Button, Card, Col, Container, Row, useAccordionButton } from "react-bootstrap";
 import { BsTrash, BsXLg } from "react-icons/bs";
 
 import '../css/tabletest.css'
 
-function SingleAccordion(props)
+function hideAccordion()
+{
+    console.log("hidden!");
+}
+
+function AccordionIconButton({ children, onClick })
+{
+    return (
+        <div type="button" onClick={onClick}>
+            {children}
+        </div>
+    );
+}
+
+function CustomToggle({ children, eventKey })
+{
+    const decoratedOnClick = useAccordionButton(eventKey);
+
+    return (
+        <div type="button" onClick={decoratedOnClick}>
+            {children}
+        </div>
+    );
+}
+
+function SingleAccordion2(props)
 {
     let accordionClass = props.accHidden ? " accordion-hidden" : "accordion-visible";
     return (
-        <Accordion>
-            <Accordion.Item eventKey="0" className={"single-accordion " + accordionClass}>
-                <Accordion.Header>
+        <Accordion className={"single-accordion " + accordionClass}>
+            <Card>
+                <Card.Header>
                     <Container>
                         <Row>
                             <Col xs="1">
-                                <BsXLg className="accordion-icon-button"/>
+                                <AccordionIconButton onClick={hideAccordion}>
+                                    <BsXLg className="accordion-icon-button"/>
+                                </AccordionIconButton>
                             </Col>
-                            <Col xs="10">
-                                {props.accTitle}
+                            <Col xs="10" on>
+                                <CustomToggle eventKey="0">{props.accTitle}</CustomToggle>
                             </Col>
                             <Col xs="1">
                                 <BsTrash className="accordion-icon-button"/>
                             </Col>
                         </Row>
                     </Container>
-                </Accordion.Header>
-                <Accordion.Body>
-                    {props.accDesc}
-                </Accordion.Body>
-            </Accordion.Item>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>{props.accDesc}</Card.Body>
+                </Accordion.Collapse>
+            </Card>
         </Accordion>
     );
 }
 
-export default SingleAccordion;
+export default SingleAccordion2;
